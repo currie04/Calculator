d0aa801f1ad08864a492c778317bb2242889ce80
currie04 <orlenamay2204@hotmail.com>
2024-09-16 15:53:33 +0100

    finished version

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ let operator_choice = "";
 let display_value = "";
 let result = 0;
 
+/*Making the buttons work*/
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator')
+const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear")
+
+/* Screen */
+const screen = document.querySelector("#display");
+
 
 function add(num1, num2){
     return num1 + num2;
@@ -26,61 +36,62 @@ function operate(one, two, operator_choice){
     switch(operator_choice){
         case '+': return add(one, two);
         case '-': return subtract(one, two);
-        case '/': return divide(one, two); 
+        case '/': 
+            if(two == 0){
+                return;
+            }else{ 
+                return divide(one, two).toFixed(2);
+            }
         case 'x': return multiply(one, two);
     }
 }
 
-/*Making the buttons work*/
 
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator')
-const equals = document.querySelector("#equals");
-const clear = document.querySelector("#clear")
-
-/* Screen */
-const screen = document.querySelector("#display");
 
 
 numbers.forEach(number => {
     number.addEventListener("click", function() {
-        if(first_number == 0 && second_number == 0 && operator_choice == ""){
-            display_value = ""
+        // If display value is currently an operator, get rid of it
+        // Ensures operator is never displayed at the same time as a number
+        if(display_value == "+" || display_value == "-" || display_value == "x" || display_value == "/"){
+            display_value = "";
         }
+
+        //If the operator has been selected add number to second number
         if(operator_choice !== ""){
-            if (second_number == 0){
-                display_value = "";
-            }
             display_value += number.textContent;
             second_number = parseInt(display_value); 
         } else {
+        //Otherwise add to first
+            if(first_number == 0){
+                display_value = "";
+            }
            display_value += number.textContent;
            first_number = parseInt(display_value); 
         } 
         screen.textContent = display_value;
-        console.log(`display_value: ${display_value}`);
-        console.log(`operator_choice : ${operator_choice}`);
-        console.log(`first_number: ${first_number}`);
-        console.log(`second_number: ${second_number}`);
     });
 })
 
+
+// if an operator is clicked - + * /
 operators.forEach(operator => {
     operator.addEventListener("click", function(){
+        /*if an operator is selected before the equals sign 
+        is pressed from the last result, operate and set the result to be the first number*/
         if(first_number != 0 && second_number != 0 && operator != ""){
             onEquals();
             first_number = result;
         }
+
+        
         if(first_number == 0){
             first_number = result;
         }
         operator_choice = operator.textContent;
         display_value = operator.textContent;
+        console.log(display_value);
         screen.textContent = display_value;
-        console.log(`display_value: ${display_value}`);
-        console.log(`operator_choice : ${operator_choice}`);
-        console.log(`first_number: ${first_number}`);
-        console.log(`second_number: ${second_number}`);
     });
 })
 
@@ -92,9 +103,9 @@ clear.addEventListener("click", function(){
     operator_choice = "";
     first_number = 0;
     second_number = 0;
-    result = 0;
     display_value = 0;
-    screen.textContent = display_value;
+    result = 0;
+    screen.textContent = "";
 });
 
 function onEquals(){
@@ -102,9 +113,13 @@ function onEquals(){
     operator_choice = "";
     first_number = 0;
     second_number = 0;
-    display_value = result;
-    screen.textContent = display_value;
-
+    if(result != undefined){
+        console.log("valid");
+        display_value = result;
+        screen.textContent = display_value;
+    } else {
+        screen.textContent = "No";
+    }
 }
 
 
